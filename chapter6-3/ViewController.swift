e0c0b7957449a15074de54dcd5fa60317a168d60
SMB開発部 <sorimachi-aws@hotmail.com>
2014-11-18 16:50:25 +0900

    Webサイトを読み込みできます

--- chapter6-3/ViewController.swift
+++ chapter6-3/ViewController.swift
@@ -8,19 +8,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController , UIWebViewDelegate{
     @IBOutlet weak var myWebView: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        //デリゲート先を設定する
+        myWebView.delegate = self
+        //MARK : URLリクエスト
+        var myURL = NSURL(string: "http://www.apple.com/jp")
+        var myURLReq = NSURLRequest(URL: myURL!)
+        myWebView.loadRequest(myURLReq)
     }
+    
+    func webViewDidStartLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = true
+    }
+    
+    func webViewDidFinishLoad(webView: UIWebView) {
+        UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-
 }
 
